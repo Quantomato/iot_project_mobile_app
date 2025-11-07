6d3b276eccda188c98fc6c1d429c96e1071d71a1
Michael Salemi <48805730+mikey241124@users.noreply.github.com>
2025-11-07 04:39:17 -0500

    Update receiver.ino
    complete rewrite, nothing was working and this simplified code functions for demo

--- arduino/receiver.ino
+++ arduino/receiver.ino
@@ -1,126 +1,88 @@
-const int PIN1 = 2; const int PIN2 = 4; const int PIN3 = 5;
-const int PIN4 = 34; const int PIN5 = 13; const int PIN6 = 14;
-const int PIN7 = 15; const int PIN8 = 16; const int PIN9 = 17;
-const int PIN10 = 18; const int PIN11 = 19; const int PIN12 = 21;
-const int PIN13 = 22; const int PIN14 = 32; const int PIN15 = 33;
+#include <ESP32Servo.h>
 
-unsigned long LD1 = 0; unsigned long LD2 = 0; unsigned long LD3 = 0; unsigned long LD4 = 0; unsigned long LD5 = 0; 
-unsigned long LD6 = 0; unsigned long LD7 = 0; unsigned long LD8 = 0; unsigned long LD9 = 0; unsigned long LD10 = 0; 
-unsigned long LD11 = 0; unsigned long LD12 = 0; unsigned long LD13 = 0; unsigned long LD14 = 0; unsigned long LD15 = 0;
+const int pins[] = {13, 18, 19, 21, 22};
 
-unsigned long lastStatusPrint = 0;  // for 2 s updates
+#define servo1Pin 25
+#define servo2Pin 26
+
+int lowered = 10;
+int raised = 120;
+
+Servo servo1;
+Servo servo2;
+
+bool bollard1Raised = false;
+bool bollard2Raised = false;
+
+bool parkingAllowed1 = false;
+bool parkingAllowed2 = false;
+
+unsigned long lastDetect[5] = {};
+unsigned long lastStatusPrint = 0;
+int count = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(PIN1, INPUT_PULLUP); pinMode(PIN2, INPUT_PULLUP); pinMode(PIN3, INPUT_PULLUP);
-  pinMode(PIN4, INPUT_PULLUP); pinMode(PIN5, INPUT_PULLUP); pinMode(PIN6, INPUT_PULLUP);
-  pinMode(PIN7, INPUT_PULLUP); pinMode(PIN8, INPUT_PULLUP); pinMode(PIN9, INPUT_PULLUP);
-  pinMode(PIN10, INPUT_PULLUP); pinMode(PIN11, INPUT_PULLUP); pinMode(PIN12, INPUT_PULLUP);
-  pinMode(PIN13, INPUT_PULLUP); pinMode(PIN14, INPUT_PULLUP); pinMode(PIN15, INPUT_PULLUP); 
+  pinMode(pins[0], INPUT_PULLUP);
+  pinMode(pins[1], INPUT_PULLUP);
+  pinMode(pins[2], INPUT_PULLUP);
+  pinMode(pins[3], INPUT_PULLUP);
+  pinMode(pins[4], INPUT_PULLUP);
 
-  Serial.println("IR receiver status monitor started.");
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
 }
 
 void loop() {
-  //constantly check for connection
-  if (digitalRead(PIN1) == LOW){
-    LD1 = millis();
-  }
-  if (digitalRead(PIN2) == LOW){
-    LD2 = millis();
-  }
-  if (digitalRead(PIN3) == LOW){
-    LD3 = millis();
-  }
-  if (digitalRead(PIN4) == LOW){
-    LD4 = millis();
-  }
-  if (digitalRead(PIN5) == LOW){
-    LD5 = millis();
-  }
-  if (digitalRead(PIN6) == LOW){
-    LD6 = millis();
-  }
-  if (digitalRead(PIN7) == LOW){
-    LD7 = millis();
-  }
-  if (digitalRead(PIN8) == LOW){
-    LD8 = millis();
-  }
-  if (digitalRead(PIN9) == LOW){
-    LD9 = millis();
-  }
-  if (digitalRead(PIN10) == LOW){
-    LD10 = millis();
-  }
-  if (digitalRead(PIN11) == LOW){
-    LD11 = millis();
-  }
-  if (digitalRead(PIN12) == LOW){
-    LD12 = millis();
-  }
-  if (digitalRead(PIN13) == LOW){
-    LD13 = millis();
-  }
-  if (digitalRead(PIN14) == LOW){
-    LD14 = millis();
-  }
-  if (digitalRead(PIN15) == LOW){
-    LD15 = millis();  
+  //if low, then pins line of sight is not broken, aka currently detected LOS
+  for (int i = 0; i < 5; i++) {
+    if (digitalRead(pins[i]) == LOW) {
+      lastDetect[i] = millis();
+    }
   }
 
+  bool LOS1 = (digitalRead(pins[0]) == LOW);
+  bool LOS2 = (digitalRead(pins[1]) == LOW);
+  bool LOS3 = (digitalRead(pins[2]) == LOW);
+  bool LOS4 = (digitalRead(pins[3]) == LOW);
+  bool LOS5 = (digitalRead(pins[4]) == LOW);
 
-  //Every 4 seconds, calculate and print out the current parking array status
-  if (millis() - lastStatusPrint >= 4000) {
+  parkingAllowed1 = (LOS1 && LOS2 && !LOS4);
+  parkingAllowed2 = (LOS2 && LOS4 && !LOS5);
+
+  //pring out current status for debugging
+  if (millis() - lastStatusPrint >= 1000) {
     lastStatusPrint = millis();
+    count++;
 
-    //checks if these have made a connection in the last 5 seeconds
-    bool LEFT1 = (millis() - LD1) < 5000;
-    bool LEFT2 = (millis() - LD2) < 5000;
-    bool LEFT3 = (millis() - LD3) < 5000;
-    bool MIDDLE1 = (millis() - LD4) < 5000;
-    bool MIDDLE2 = (millis() - LD5) < 5000;
-    bool MIDDLE3 = (millis() - LD6) < 5000;
-    bool RIGHT1 = (millis() - LD7) < 5000;
-    bool RIGHT2 = (millis() - LD8) < 5000;
-    bool RIGHT3 = (millis() - LD9) < 5000;
-    bool FRONT1 = (millis() - LD10) < 5000;
-    bool FRONT2 = (millis() - LD11) < 5000;
-    bool FRONT3 = (millis() - LD12) < 5000;
-    bool FRONT4 = (millis() - LD13) < 5000;
-    bool CEN1 = (millis() - LD14) < 5000;
-    bool CEN2 = (millis() - LD15) < 5000;
+    Serial.println("---- IR Status ----");
+    for (int i = 0; i < 5; i++) {
+      Serial.print(" Pin ["); Serial.print(pins[i]); Serial.print("] : ");
+      bool seen = (millis() - lastDetect[i]) < 2000;
+      if(seen){
+        Serial.print("✅");
+      } else{
+        Serial.print("❌");
+      }
+    }
+    Serial.println("\n-------------------");
 
-    //sorts all led connections into rows
-    bool LEFT = LEFT1 && LEFT2 && LEFT3;
-    bool MIDDLE = MIDDLE1 && MIDDLE2 && MIDDLE3; 
-    bool RIGHT = RIGHT1 && RIGHT2 && RIGHT3;
-    bool FRONTL = FRONT1 && FRONT2;
-    bool FRONTR = FRONT3 && FRONT4;
+    if (count > 4) {
+      count = 0;
+      Serial.println("\nSANITY CHECK\n");
+    }
+  }
 
-    //validates if the parking spots are valid for raising bollards
-    bool PARKINGL = LEFT && MIDDLE && FRONTL && !CEN1;
-    bool PARKINGR = RIGHT && MIDDLE && FRONTR && !CEN2;
+  if(bollard1Raised && parkingAllowed1){
+    servo1.write(raised);
+  } else{
+    servo1.write(lowered);
+  }
 
-    //serial printing for debugging purposes
-    Serial.print("Left: ");
-    Serial.print(LEFT ? "O" : "X");
-    Serial.print(" | Middle: ");
-    Serial.println(MIDDLE ? "O" : "X");
-    Serial.print("| Right: ");
-    Serial.print(RIGHT ? "O" : "X");
-    Serial.print(" | Front Left ");
-    Serial.println(FRONTL ? "O" : "X");
-    Serial.print("| Front Right ");
-    Serial.print(FRONTR ? "O" : "X");
-    Serial.print(" | Center Left ");
-    Serial.println(CEN1 ? "O" : "X");
-    Serial.print(" | Center Right ");
-    Serial.println(CEN2 ? "O" : "X");
-    Serial.print(" | Parking Space Left ");
-    Serial.println(PARKINGL ? "O" : "X");
-    Serial.print(" | Parking Space Right ");
-    Serial.println(PARKINGR ? "O" : "X");
+  if(bollard2Raised && parkingAllowed2){
+    servo2.write(raised);
+  } else{
+    servo2.write(lowered);
   }
 }
