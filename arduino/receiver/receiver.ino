7d30c54991f6baacb9a92ba8b426f339e3ed6d6a
Quantomato <juansebastianlomanto@yahoo.com>
2025-11-07 06:05:13 -0500

    Mayhaps finished arduino code?
    Definitely did not lmao.

--- arduino/receiver/receiver.ino
+++ arduino/receiver/receiver.ino
@@ -1,8 +1,8 @@
 #include <ESP32Servo.h>
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
-#include <SPIFFS.h>
 #include <addons/TokenHelper.h>
+#include <ArduinoJson.h>
 
 const int pins[] = {13, 18, 19, 21, 22};
 
@@ -22,6 +22,10 @@ const char* password = "whatthehelly";
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
+
+const char* project_id = "bollardbros-1afdb";
+const char* client_email = "firebase-adminsdk-fbsvc@bollardbros-1afdb.iam.gserviceaccount.com";
+const char* private_key = "-----BEGIN PRIVATE KEY-----\n"MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC8VhBK8HLAEbCg\nvHQxE9vV3n3WXpouMR03/20yXL6cIWdZfRyULQXofAnxjHVIPRShl0QtqUYZE05I\nB6WsXGlP8gDCdF7tEYFYvRfOljaGkNzF++veaL/p2pJXeI/pyIGFwyZ04VC6ONjY\nAmgT9ijRR2EhL5RtyuvWBihRaEIKdwSmX9DHuqru/a4+21tTva2/nn40o1YqDedG\nSxSpR7GEEsW3tHRdRrYlkm6pafrvBfTeyDh7CMLeS+hNNPLK72slr/lNWXaCLDJk\nryTNdZxrxFmk6NqH9l8XJCX/+043Q/xFeqnVu3O0OgCmRS9XRRNv4ED0Y2WnRaUA\naxTKRmj7AgMBAAECggEAAWJ5RIZyXt13w3lvOex6nslqem4qoFv7FNjQvYs3WYrY\nqYHvzlprDd5aQlzf1d6hGAkL1sbE/aqJ1ky8F+wmrWWgQiMR5ENPL08AmE5mwI7b\nGKzf+zwg68AQIwrUtV7PLk5qKiIn8kk4YD+FnV6fcTPJpTFQ3DfJi8LIKW+59egO\nivYQGibYPO2ZMHaXwAPgw04F2SEVMGnyH90CIg48vECiwH5OTwKOZO2sScAEPi6x\n6OZWcc3DWM38oIBWZgFe5NpcAflpkjoNEae2ZUHkQYKTyQoGyNACpjT4P9eJmmGl\nVusVZDCU8hl3vtv8a05ce3wUxrw+9F13G6BzmFwFAQKBgQDckit3szLaKLCks7XL\n90XgLs2JulSZRP/fC6ZYjoNnYqQPsQXmUkqqc7gPmJwGJQ7wrCwt3oUnTfOYyoU7\nPmGO6W3UWrVYmsUqzM7ljcuIFQVZ4DrURh2D1NYnKlOfUmemUdXHBS7WGmncwVrN\nCncxrHdNAtSy6QRnHnhRLI4RAQKBgQDalmgHJTTQRoVLCc0pYc0x+T4lMDozu0aY\nfbHA8NzXMoCvOkazmQDKLOcYnsQ4KKiZnvPgvYWQdYOPRfWjAA7un6eb3wZ48tle\nWhhcfIXxlzYIB7ldNgB1fMYBPKoDxML7PePyV8fIt5yvoWfNBoXp/eiBGjC7zg85\nlVID6m69+wKBgGk1X35s/2iH2Bn3jlOH0tCoAmobcpbZDqM5T1MWRx9dMZw6d/Hw\ndqTvJXBq/qD/tCygM0J9zU1+JR9QGlSz4JzmQvjze2SUDk/n+RgNlnOGc1L3XZg7\nCYEEWFG+Irzv9XijuDCCDGEboDsoD0OWJlyPQyh8nS/pbawyIHm3otwBAoGAQD3m\nEvuHWxLux8/4oQniYaaL8igLYvrOU7DVVgw0C3cHRPiOoOf9yG2L+btnlc21R3jk\n2g85OuyenrQD7kTtHoqDmxbf8xoEcmtuL3hVcVxsf82tV8ywM9dY5jIrEXJ5j5dv\n7+CGkeIMxb1dIW+iWdPKym6lFf0IknblJMt4gpMCgYBdiahUPvh2k8JRURwVZLNa\nJmkO33mO9QbH1cvPt/x5odBpzzpT0WaM6BMxZ5EpxDR42DPo9/Iw9CIJ/o7GnRQE\nlz835j5gKOrPrHDa+XzRwyQbk60AH0HVkbrONZeYqP+/4R7nNms9SOA6X0mci6fb\n1+3gI3+Cja/b6Q85t9kuIQ=="\n-----END PRIVATE KEY-----\n";
 
 void updateFirestoreBollard(bool canRaiseBollard, int index);
 
@@ -44,30 +48,9 @@ void setup() {
   }
   Serial.println("\nConnected to WiFi!");
 
-  // Mount SPIFFS
-  if(!SPIFFS.begin(true)){
-    Serial.println("Failed to mount SPIFFS");
-    return;
-  }
-
-  // Read JSON file ---------------------------------------------------------------------------------------------------------------------------
-  File file = SPIFFS.open("/espDatabaseInfo.json", "r");
-  if(!file){
-    Serial.println("Failed to open espDatabaseInfo.json");
-    return;
-  }
-
-  DynamicJsonDocument doc(4096);
-  DeserializationError error = deserializeJson(doc, file);
-  file.close();
-  if (error) {
-    Serial.println("Failed to parse espDatabaseInfo.json");
-    return;
-  }
-
-  config.service_account.data.project_id = doc["project_id"].as<String>().c_str();
-  config.service_account.data.client_email = doc["client_email"].as<String>().c_str();
-  config.service_account.data.private_key = doc["private_key"].as<String>().c_str();
+  config.service_account.data.project_id = project_id;
+  config.service_account.data.client_email = client_email;
+  config.service_account.data.private_key = private_key;
   config.token_status_callback = tokenStatusCallback;
   config.max_token_generation_retry = 5;
 
